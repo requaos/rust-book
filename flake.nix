a636a5b78f870ae71055faf7785e5994ad84afd9
Neil Skinner <reqpro@requaos.com>
2023-02-18 16:53:18 +0000

    starting

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "ironpress";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    # nixpkgs.follows = "std/nixpkgs";
+    std.url = "github:divnix/std";
+    std.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, std, ... } @ inputs: std.growOn
+    {
+      inherit inputs;
+      systems = [ "x86_64-linux" ];
+      cellsFrom = ./nix;
+      cellBlocks = with std.blockTypes; [
+        (devshells "devshells" { ci.build = true; })
+        (installables "packages" { ci.build = true; })
+        (runnables "packages" { ci.build = true; })
+      ];
+    }
+    {
+      devShells = std.harvest inputs.self [ "devshells" "devshells" ];
+    };
+}
---
+++ nix/devshells/devshells.nix
@@ -0,0 +1,70 @@
+{ inputs, cell, ... }:
+let
+  inherit (inputs) nixpkgs;
+  inherit (inputs.std) std lib;
+
+  fenix = inputs.fenix.packages;
+
+  l = nixpkgs.lib // builtins;
+
+  rust-toolchain = with fenix; combine [
+    complete.cargo
+    complete.clippy
+    complete.rustc
+    complete.rust-src
+    complete.rustfmt
+    complete.rust-analyzer
+    targets.wasm32-unknown-unknown.latest.rust-std
+  ];
+
+  dev = lib.dev.mkShell {
+    devshell.packages = with nixpkgs; [
+      openssl
+      openssl.dev
+      clang
+      llvm
+      llvmPackages.libclang
+      pkgconfig
+      nodePackages.sass
+    ]
+    ++
+    [
+      rust-toolchain
+    ];
+
+    env = [{
+      name = "RUST_SRC_PATH";
+      value = "${rust-toolchain}/lib/rustlib/src/rust/library";
+    }
+    {
+      name = "LIBCLANG_PATH";
+      value = l.makeLibraryPath [ nixpkgs.llvmPackages.libclang.lib ];
+    }
+    {
+      name = "PKG_CONFIG_PATH";
+      value = "${nixpkgs.openssl.dev.outPath}/lib/pkgconfig:" + "$PKG_CONFIG_PATH";
+    }
+    ];
+
+
+    commands = [
+      {
+        package = nixpkgs.treefmt;
+        category = "repo tools";
+      }
+      {
+        package = nixpkgs.alejandra;
+        category = "repo tools";
+      }
+      {
+        package = std.cli.default;
+        category = "std";
+      }
+    ]
+    ;
+  };
+in
+{
+  inherit dev;
+  default = dev;
+}
